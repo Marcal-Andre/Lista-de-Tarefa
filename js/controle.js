d5945cb93c5beb21c309c8a9a6d4e2ea7534582e
Your Name <andremarcal.dev@gmail.com>
2025-01-26 09:26:52 -0300

    acertan.js

--- js/controle.js
+++ js/controle.js
@@ -1,6 +1,9 @@
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 
+
+    //aqui vamos estruturar as funcoes
+
 function addTarefa(){
     //Pegar o Valor digitado no Input
 }
