c80649e3409cb509938174038233726c0a299fdc
Your Name <andremarcal.dev@gmail.com>
2025-01-27 02:47:45 -0300

    up.js

--- js/controle.js
+++ js/controle.js
@@ -1,13 +1,33 @@
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
+let main = documentElementById('areaLista');
 
 
-    //aqui vamos estruturar as funcoes
+    
 
 function addTarefa(){
     //Pegar o Valor digitado no Input
+    let valorInput = input.value;
+
+    //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+    if ((valorInput !=="") && (valorInput !== null) && (valorInput !== undefined)){
+
+        let novoItem =` <div class="item">
+                <div class="item-icone">
+                    <i class="mdi mdi-circle-outline"></i>
+    
+                </div>
+                <div class="item-nome">
+                    Teste de Tarefa
+    
+                </div>
+                <div class="item-botao">
+                    <button class="delete"> <i class="mdi mdi-delete"></i>Deletar</button>
+    
+                </div>`;
+
+                main.innerHTML += novoItem;
+    }
 
 
 }
-
-    //criando mecanismo de logica
